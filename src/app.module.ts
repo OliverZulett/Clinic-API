91c23c0037e32fac7b5537ae8ec4adeba07f8c2f
root <root@DESKTOP-PV8QB67.localdomain>
2022-02-22 22:55:28 -0400

    db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.prod.env', '.test.env', '.env'
+      ]
+    }),
+    DatabaseModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
